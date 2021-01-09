41516b8792754e7dd17cf07fd6fc2543c3b36b67
jodavis <jodavis42@gmail.com>
2021-01-08 17:16:36 -0800

    Made helper types for Vector/Matrix to extract parameters

---
+++ CSShaders/Core/ExtendedTypes/MatrixType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSShaders
+{
+  /// <summary>
+  /// A helper class to fetch information about a matrix.
+  /// </summary>
+  public class MatrixType
+  {
+    /// <summary>
+    /// Load this shader type as a matrix. If this isn't a matrix, null is returned.
+    /// </summary>
+    public static MatrixType Load(ShaderType type)
+    {
+      if (type == null || type.mBaseType != OpType.Vector)
+        return null;
+
+      return new MatrixType() { Type = type };
+    }
+
+    public ShaderType GetComponentType()
+    {
+      return Type.mParameters[0] as ShaderType;
+    }
+
+    public UInt32 GetComponentCount()
+    {
+      var constantLiteral = Type.mParameters[1] as ShaderConstantLiteral;
+      if (constantLiteral == null)
+        throw new Exception("Parameter should be a constant literal");
+      return (UInt32)constantLiteral.mValue;
+    }
+
+    public ShaderType Type;
+  }
+}
---
+++ CSShaders/Core/ExtendedTypes/VectorType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSShaders
+{
+  /// <summary>
+  /// A helper class to fetch information about a vector.
+  /// </summary>
+  public class VectorType
+  {
+    /// <summary>
+    /// Load this shader type as a vector. If this isn't a vector, null is returned.
+    /// </summary>
+    public static VectorType Load(ShaderType type)
+    {
+      if (type == null || type.mBaseType != OpType.Vector)
+        return null;
+
+      return new VectorType() { Type = type };
+    }
+
+    public ShaderType GetComponentType()
+    {
+      return Type.mParameters[0] as ShaderType;
+    }
+
+    public UInt32 GetComponentCount()
+    {
+      var constantLiteral = Type.mParameters[1] as ShaderConstantLiteral;
+      if (constantLiteral == null)
+        throw new Exception("Parameter should be a constant literal");
+      return (UInt32)constantLiteral.mValue;
+    }
+
+    public ShaderType Type;
+  }
+}
