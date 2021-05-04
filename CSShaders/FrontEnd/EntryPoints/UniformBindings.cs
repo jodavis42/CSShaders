74a52a145e970761bc2f906ae079a2dee3710720
jodavis <jodavis42@gmail.com>
2021-05-04 16:41:17 -0700

    Improved comparisons for uniform bindings

--- CSShaders/FrontEnd/EntryPoints/UniformBindings.cs
+++ CSShaders/FrontEnd/EntryPoints/UniformBindings.cs
@@ -12,11 +12,25 @@ namespace CSShaders
       return Tuple.Create(DescriptorSet, BindingId).GetHashCode();
     }
 
-    public override bool Equals(object obj)
+    public bool Equals(UniformBindings rhs)
     {
-      if (obj is UniformBindings uniformBindings)
-        return uniformBindings.DescriptorSet.Equals(DescriptorSet) & uniformBindings.BindingId.Equals(BindingId);
-      return false;
+      if (rhs is null)
+        return false;
+      return rhs.DescriptorSet.Equals(DescriptorSet) & rhs.BindingId.Equals(BindingId);
     }
+    public override bool Equals(object obj) => this.Equals(obj as UniformBindings);
+
+    public static bool operator ==(UniformBindings lhs, UniformBindings rhs)
+    {
+      if (lhs is null)
+      {
+        if (rhs is null)
+          return true;
+        return false;
+      }
+      return lhs.Equals(rhs);
+    }
+
+    public static bool operator !=(UniformBindings lhs, UniformBindings rhs) => !(lhs == rhs);
   }
 }
