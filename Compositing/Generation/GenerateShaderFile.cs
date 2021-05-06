c98ed63d9a9df64063372934ec469fb44c1ea3b0
jodavis <jodavis42@gmail.com>
2021-05-05 19:13:55 -0700

    Added the core compositor functionality. This includes generating a definition and then emit the generated code.

---
+++ Compositing/Generation/GenerateShaderFile.cs
@@ -0,0 +1,10 @@
+﻿namespace CSShaders.Compositing
+{
+  /// The source code for one shader file after composite generation. This file might be the source
+  /// to a shader or it could be a file for some common shared data (such as a uniform buffer declaration).
+  public class GenerateShaderFile
+  {
+    public string FileName;
+    public string Code;
+  }
+}
