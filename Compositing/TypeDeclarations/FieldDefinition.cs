a97fac885c12107b24614660782d1858bb1d54a0
jodavis <jodavis42@gmail.com>
2021-05-05 19:09:50 -0700

    Added a bunch of type declaration types for the compositor

---
+++ Compositing/TypeDeclarations/FieldDefinition.cs
@@ -0,0 +1,28 @@
+﻿namespace CSShaders.Compositing
+{
+  /// Represents the definition of a field that may or may not yet exist as a shader type (we could be generating it now).
+  public class FieldDefinition
+  {
+    public ShaderAttributes Attributes = new ShaderAttributes();
+    /// The type of this field
+    public TypeDefinition Type;
+    /// The name of this field
+    public string Name;
+    /// The type that owns this field
+    public TypeDefinition Owner;
+    /// If this was created from a ShaderField, this will be that field. Otherwise this will be null.
+    public ShaderField ShaderField;
+
+    public FieldDefinition Clone(TypeDefinition owner = null)
+    {
+      var clone = new FieldDefinition
+      {
+        Attributes = this.Attributes.Clone(),
+        Type = this.Type,
+        Name = this.Name,
+        Owner = owner,
+      };
+      return clone;
+    }
+  }
+}
