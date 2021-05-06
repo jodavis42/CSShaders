5a9a7a0122f9e266c60d6491d2ffe897977b4a44
jodavis <jodavis42@gmail.com>
2021-05-05 18:12:20 -0700

    Added a helper to parse some attributes. Also added a code builder helper.

---
+++ CSShaders/Utilities/AttributeExtensions.cs
@@ -0,0 +1,65 @@
+﻿namespace CSShaders
+{
+  public static class AttributeExtensions
+  {
+    public static Shader.StageInput ParseStageInput(ShaderAttribute attribute)
+    {
+      var parsedAttribute = new Shader.StageInput() { Location = -1, Component = -1 };
+      // Parse from the C# attribute if it exists.
+      if (attribute.Attribute != null)
+      {
+        foreach (var namedArg in attribute.Attribute.NamedArguments)
+        {
+          if (namedArg.Key == nameof(Shader.StageInput.Name))
+            parsedAttribute.Name = namedArg.Value.ToString();
+          else if (namedArg.Key == nameof(Shader.StageInput.Location))
+            parsedAttribute.Location = (int)namedArg.Value.Value;
+          else if (namedArg.Key == nameof(Shader.StageInput.Component))
+            parsedAttribute.Location = (int)namedArg.Value.Value;
+        }
+      }
+      // Also always check pre-parsed attributes
+      foreach (var namedArg in attribute.Parameters)
+      {
+        if (namedArg.Name == nameof(Shader.StageInput.Name))
+          parsedAttribute.Name = namedArg.Value.ToString();
+        else if (namedArg.Name == nameof(Shader.StageInput.Location))
+          parsedAttribute.Location = (int)namedArg.Value;
+        else if (namedArg.Name == nameof(Shader.StageInput.Component))
+          parsedAttribute.Location = (int)namedArg.Value;
+      }
+
+      return parsedAttribute;
+    }
+
+    public static Shader.StageOutput ParseStageOutput(ShaderAttribute attribute)
+    {
+      var parsedAttribute = new Shader.StageOutput() { Location = -1, Component = -1 };
+      // Parse from the C# attribute if it exists.
+      if (attribute.Attribute != null)
+      {
+        foreach (var namedArg in attribute.Attribute.NamedArguments)
+        {
+          if (namedArg.Key == nameof(Shader.StageInput.Name))
+            parsedAttribute.Name = namedArg.Value.ToString();
+          else if (namedArg.Key == nameof(Shader.StageOutput.Location))
+            parsedAttribute.Location = (int)namedArg.Value.Value;
+          else if (namedArg.Key == nameof(Shader.StageOutput.Component))
+            parsedAttribute.Location = (int)namedArg.Value.Value;
+        }
+      }
+      // Also always check pre-parsed attributes
+      foreach (var namedArg in attribute.Parameters)
+      {
+        if (namedArg.Name == nameof(Shader.StageInput.Name))
+          parsedAttribute.Name = namedArg.Value.ToString();
+        else if (namedArg.Name == nameof(Shader.StageOutput.Location))
+          parsedAttribute.Location = (int)namedArg.Value;
+        else if (namedArg.Name == nameof(Shader.StageOutput.Component))
+          parsedAttribute.Location = (int)namedArg.Value;
+      }
+
+      return parsedAttribute;
+    }
+  }
+}
