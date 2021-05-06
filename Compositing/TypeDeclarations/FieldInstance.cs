61f92be12dac2bc6fa7e6bfd0268fc21b77e4e89
jodavis <jodavis42@gmail.com>
2021-05-05 19:09:50 -0700

    Added a bunch of type declaration types for the compositor

---
+++ Compositing/TypeDeclarations/FieldInstance.cs
@@ -0,0 +1,20 @@
+﻿namespace CSShaders.Compositing
+{
+  /// A field instance represents the instance of a field on a varaible. The just allows the original field definition to be tracked.
+  public class FieldInstance : VariableInstance
+  {
+    public FieldDefinition Field;
+
+    public FieldInstance Clone(VariableInstance owner = null)
+    {
+      var clone = new FieldInstance()
+      {
+        Type = this.Type,
+        Name = this.Name,
+        Owner = owner,
+        Field = this.Field.Clone(),
+      };
+      return clone;
+    }
+  }
+}
