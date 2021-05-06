7b715966613541c5798fe100712b834044ab9e04
jodavis <jodavis42@gmail.com>
2021-05-05 19:03:22 -0700

    Added a test step to write a shader library out to a spirv binary file

---
+++ CSShadersTests/TestRunner/Steps/ShaderLibraryBinaryWriterStep.cs
@@ -0,0 +1,40 @@
+﻿using CSShaders;
+using System.IO;
+
+namespace CSShadersTests
+{
+  class ShaderLibraryBinaryWriterStep : Step
+  {
+    public const string DefaultLibraryKey = "Library";
+    public string LibraryKey = DefaultLibraryKey;
+
+    public const string DefaultTranslatorKey = "Translator";
+    public string TranslatorKey = DefaultTranslatorKey;
+
+    public const string DefaultSpvPathKey = TestConfigStep.SpvBinaryPathKey;
+    public string SpvPathKey = DefaultSpvPathKey;
+
+    public override StepResult Run(Blackboard blackboard)
+    {
+      var logger = blackboard.Get<ILogger>("Logger");
+
+      var shaderLibrary = blackboard.Get<ShaderLibrary>(LibraryKey);
+      if (shaderLibrary == null)
+      {
+        logger.Log($"Missing ShaderLibrary key {LibraryKey}", ILogger.Verbosity.Error);
+        return StepResult.Failed;
+      }
+
+      var translator = blackboard.Get<FrontEndTranslator>(TranslatorKey);
+      var spvPath = blackboard.Get<string>(SpvPathKey);
+
+      var writer = new BinaryWriter(new FileStream(spvPath, FileMode.Create));
+      var spirvWriter = new SpirVStreamWriter(writer);
+      var spirVBinaryBackend = new ShaderToSpirVBinary();
+      spirVBinaryBackend.Write(spirvWriter, shaderLibrary, translator);
+      writer.Close();
+
+      return StepResult.Success;
+    }
+  }
+}
