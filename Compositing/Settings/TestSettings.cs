155c8a0e7725ca68711cd3d752f012e4ce367c6f
jodavis <jodavis42@gmail.com>
2021-05-05 19:07:48 -0700

    Added the compositor project and some basic settings declarations

---
+++ Compositing/Settings/TestSettings.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSShaders.Compositing
+{
+  /// A collection of settings mostly for testing. These settings also show how settings can be created
+  public class TestSettings
+  {
+    class Context
+    {
+      internal List<ShaderType> Int32Types = new List<ShaderType>();
+      internal List<ShaderType> FloatTypes = new List<ShaderType>();
+      internal ShaderType Float4x4Type;
+    }
+
+    public static CompositorSettings BuildTestCompositorSettings(ShaderModule module)
+    {
+      var context = new Context();
+      context.Int32Types.Add(module.FindType(new TypeKey(typeof(Int32))));
+      context.Int32Types.Add(module.FindType(new TypeKey(typeof(Math.Integer2))));
+      context.Int32Types.Add(module.FindType(new TypeKey(typeof(Math.Integer3))));
+      context.Int32Types.Add(module.FindType(new TypeKey(typeof(Math.Integer4))));
+      context.FloatTypes.Add(module.FindType(new TypeKey(typeof(float))));
+      context.FloatTypes.Add(module.FindType(new TypeKey(typeof(Math.Vector2))));
+      context.FloatTypes.Add(module.FindType(new TypeKey(typeof(Math.Vector3))));
+      context.FloatTypes.Add(module.FindType(new TypeKey(typeof(Math.Vector4))));
+      context.Float4x4Type = module.FindType(new TypeKey(typeof(Math.Float4x4)));
+
+      var compositorSettings = new CSShaders.Compositing.CompositorSettings();
+      compositorSettings.HardwareBuiltInSettings = CreateBasicHardwareBuiltInSettings(module, context);
+      compositorSettings.UniformSettings = CreateBasicUniformSettings(module, context);
+      compositorSettings.AttributeSettings = CreateBasicAttributeSettings(module, context);
+      compositorSettings.RenderTargetSettings = CreateBasicRenderTargetSettings(module, context);
+      compositorSettings.Lock();
+
+      return compositorSettings;
+    }
+
+    static UniformDescriptionSettings CreateBasicUniformSettings(ShaderModule module, Context context)
+    {
+      var floatType = context.FloatTypes[0];
+      var float2Type = context.FloatTypes[1];
+      var float4x4Type = context.Float4x4Type;
+
+      var uniformSettings = new UniformDescriptionSettings();
+      var frameDataDesc = uniformSettings.CreateBuffer("FrameData", new UniformBindings { BindingId = 0, DescriptorSet = 0 });
+      frameDataDesc.Add(floatType, "FrameTime");
+      frameDataDesc.Add(floatType, "LogicTime");
+
+      var cameraDataDesc = uniformSettings.CreateBuffer("CameraData", new UniformBindings { BindingId = 1, DescriptorSet = 0 });
+      cameraDataDesc.Add(floatType, "NearPlane");
+      cameraDataDesc.Add(floatType, "FarPlane");
+      cameraDataDesc.Add(float2Type, "ViewportSize");
+
+      var transformDataDesc = uniformSettings.CreateBuffer("TransformData", new UniformBindings { BindingId = 2, DescriptorSet = 0 });
+      transformDataDesc.Add(float4x4Type, "LocalToWorld");
+      transformDataDesc.Add(float4x4Type, "WorldToView");
+      transformDataDesc.Add(float4x4Type, "ViewToPerspective");
+      transformDataDesc.Add(float4x4Type, "LocalToPerspective");
+      return uniformSettings;
+    }
+
+    static HardwareBuiltInSettings CreateBasicHardwareBuiltInSettings(ShaderModule module, Context context)
+    {
+      var int32Type = context.Int32Types[0];
+      var floatType = context.FloatTypes[0];
+      var float2Type = context.FloatTypes[1];
+      var float4Type = context.FloatTypes[3];
+
+      // Now create hardware built-ins. These perform name/type matching and then generate the appropriate attribute using the actual spv.BuiltIn enum.
+      // The name matching allows the attributes to work more nicely with the generic [Input]/[Output] attributes.
+      var hardwareBuiltInSettings = new HardwareBuiltInSettings();
+      var vertexHWBISettings = hardwareBuiltInSettings.GetOrCreateFragmentDesciptions(FragmentType.Vertex);
+      vertexHWBISettings.AddInput(int32Type, "VertexIndex", Shader.HardwareBuiltinType.VertexIndex);
+      vertexHWBISettings.AddInput(int32Type, "InstanceId", Shader.HardwareBuiltinType.InstanceId);
+      vertexHWBISettings.AddOutput(float4Type, "ApiPerspectivePosition", Shader.HardwareBuiltinType.Position);
+      vertexHWBISettings.AddOutput(floatType, "PointSize", Shader.HardwareBuiltinType.PointSize);
+
+      var pixelHWBISettings = hardwareBuiltInSettings.GetOrCreateFragmentDesciptions(FragmentType.Pixel);
+      pixelHWBISettings.AddInput(float4Type, "FragCoord", Shader.HardwareBuiltinType.FragCoord);
+      pixelHWBISettings.AddInput(float2Type, "PointCoord", Shader.HardwareBuiltinType.PointCoord);
+      pixelHWBISettings.AddOutput(floatType, "FragDepth", Shader.HardwareBuiltinType.FragDepth);
+      return hardwareBuiltInSettings;
+    }
+
+    static AttributeSettings CreateBasicAttributeSettings(ShaderModule module, Context context)
+    {
+      var float2Type = context.FloatTypes[1];
+      var float3Type = context.FloatTypes[2];
+      var float4Type = context.FloatTypes[3];
+      var int4Type = context.Int32Types[3];
+
+      var attributeSettings = new AttributeSettings();
+      attributeSettings.AddAttribute(float2Type, "Uv", 0);
+      attributeSettings.AddAttribute(float3Type, "LocalNormal", 1);
+      attributeSettings.AddAttribute(float3Type, "LocalPosition", 2);
+      attributeSettings.AddAttribute(float4Type, "Color", 3);
+      attributeSettings.AddAttribute(float4Type, "Aux0", 4);
+      attributeSettings.AddAttribute(int4Type, "BoneIndices", 5);
+      return attributeSettings;
+    }
+
+    static RenderTargetSettings CreateBasicRenderTargetSettings(ShaderModule module, Context context)
+    {
+      var float4Type = context.FloatTypes[3];
+      var renderTargetSettings = new RenderTargetSettings();
+      for(var i = 0; i < 8; ++i)
+        renderTargetSettings.AddNamedRenderTarget(float4Type, $"Target{i}", i);
+      return renderTargetSettings;
+    }
+  }
+}
---
+++ Compositing/Settings/UniformDescription.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSShaders.Compositing
+{
+  using UniformBindings = CSShaders.UniformBindings;
+  public class UniformBufferField
+  {
+    public ShaderType FieldType { get; internal set; }
+    public string FieldName { get; internal set; }
+    public UniformBufferDescription Owner { get; internal set; }
+  }
+
+  /// The description of one uniform buffer. Fields are expected to be laid out in order.
+  /// Currently no location bindings are allowed, the layout is auto determined from the size and order of the fields.
+  public class UniformBufferDescription
+  {
+    /// Once locked, a buffer should not be changed. This allows caching and other optimizations.
+    public bool Locked { get; private set; }
+
+    internal string BufferName { get; private set; }
+
+    internal UniformBindings Bindings = new UniformBindings();
+    internal List<UniformBufferField> Fields = new List<UniformBufferField>();
+
+    public UniformBufferDescription(string bufferName)
+    {
+      BufferName = bufferName;
+    }
+
+    public void SetBindings(UniformBindings bindings)
+    {
+      VerifyNotLocked();
+      Bindings = bindings;
+    }
+
+    public void Add(UniformBufferField field)
+    {
+      VerifyNotLocked();
+      if (field.Owner != null)
+        throw new Exception("Cannot add field to multiple buffers");
+      field.Owner = this;
+      Fields.Add(field);
+    }
+
+    public void Add(ShaderType fieldType, string fieldName)
+    {
+      Add(new UniformBufferField { FieldType = fieldType, FieldName = fieldName });
+    }
+
+    public void Lock()
+    {
+      VerifyNotLocked();
+      Locked = true;
+    }
+
+    void VerifyNotLocked()
+    {
+      if (Locked)
+        throw new System.Exception("Cannot add a field to a locked buffer");
+    }
+  }
+
+  /// The collection of descriptions about uniform buffers that the user wants.
+  /// These control how the [HardwareBuiltIn] attribute is grouped together into actual uniform buffers.
+  public class UniformDescriptionSettings
+  {
+    /// Once locked, a buffer should not be changed. This allows caching and other optimizations.
+    public bool Locked { get; private set; }
+    public List<UniformBufferDescription> BufferDescriptions { get; private set; } = new List<UniformBufferDescription>();
+
+    /// Create a new buffer with given C# name and the specified bindings
+    public UniformBufferDescription CreateBuffer(string bufferName, UniformBindings bindings)
+    {
+      var existingBuffer = FindBuffer(bindings);
+      if (existingBuffer != null)
+        throw new Exception($"Buffer binding BindingId:{bindings.BindingId} DescriptorSet:{bindings.DescriptorSet} already exists. Cannot bind twice");
+
+      var desc = new UniformBufferDescription(bufferName);
+      desc.SetBindings(bindings);
+      BufferDescriptions.Add(desc);
+      return desc;
+    }
+
+    public UniformBufferDescription FindBuffer(UniformBindings bindings)
+    {
+      foreach(var bufferDescription in BufferDescriptions)
+      {
+        if (bufferDescription.Bindings == bindings)
+          return bufferDescription;
+      }
+      return null;
+    }
+
+    public void Lock()
+    {
+      VerifyNotLocked();
+      Locked = true;
+      foreach (var buffer in BufferDescriptions)
+        buffer.Lock();
+    }
+
+    void VerifyNotLocked()
+    {
+      if (Locked)
+        throw new System.Exception("Cannot add a field to a locked description");
+    }
+  }
+}
