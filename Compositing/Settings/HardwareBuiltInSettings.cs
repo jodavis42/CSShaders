b163639657035d37839ff66233dbc25c04a582f7
jodavis <jodavis42@gmail.com>
2021-05-05 19:07:48 -0700

    Added the compositor project and some basic settings declarations

---
+++ Compositing/Settings/HardwareBuiltInSettings.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+namespace CSShaders.Compositing
+{
+  public class HardwareBuiltInField
+  {
+    public ShaderType FieldType { get; internal set; }
+    public string FieldName { get; internal set; }
+    public Shader.HardwareBuiltinType BuiltInType { get; internal set; }
+    public HardwareBuiltInDescriptions Owner { get; internal set; }
+  }
+
+  /// The description of a hardware builtin per stage.
+  /// EVer field specifies a Name/Type pair to register to a built-in in spirv. No type validation is currently performed.
+  public class HardwareBuiltInDescriptions
+  {
+    /// Once locked, a buffer should not be changed. This allows caching and other optimizations.
+    public bool Locked { get; private set; }
+    public List<HardwareBuiltInField> Inputs { get; } = new List<HardwareBuiltInField>();
+    public List<HardwareBuiltInField> Outputs { get; } = new List<HardwareBuiltInField>();
+
+    public void AddInput(ShaderType fieldType, string fieldName, Shader.HardwareBuiltinType builtInType)
+    {
+      Add(Inputs, new HardwareBuiltInField { FieldType = fieldType, FieldName = fieldName, BuiltInType = builtInType });
+    }
+
+    public void AddOutput(ShaderType fieldType, string fieldName, Shader.HardwareBuiltinType builtInType)
+    {
+      Add(Outputs, new HardwareBuiltInField { FieldType = fieldType, FieldName = fieldName, BuiltInType = builtInType });
+    }
+
+    private void Add(List<HardwareBuiltInField> fields, HardwareBuiltInField field)
+    {
+      VerifyNotLocked();
+      field.Owner = this;
+      fields.Add(field);
+    }
+
+    public void Lock()
+    {
+      VerifyNotLocked();
+      Locked = true;
+    }
+
+    void VerifyNotLocked()
+    {
+      if (Locked)
+        throw new System.Exception("Cannot add a field to a locked description");
+    }
+  }
+
+  public class HardwareBuiltInSettings
+  {
+    /// Once locked, a buffer should not be changed. This allows caching and other optimizations.
+    public bool Locked { get; private set; }
+    Dictionary<FragmentType, HardwareBuiltInDescriptions> StageDescriptions { get; } = new Dictionary<FragmentType, HardwareBuiltInDescriptions>();
+
+    public HardwareBuiltInDescriptions GetFragmentDesciptions(FragmentType fragmentType)
+    {
+      return StageDescriptions.GetValueOrDefault(fragmentType);
+    }
+
+    public HardwareBuiltInDescriptions GetOrCreateFragmentDesciptions(FragmentType fragmentType)
+    {
+      VerifyNotLocked();
+
+      var result = GetFragmentDesciptions(fragmentType);
+      if (result == null)
+      {
+        result = new HardwareBuiltInDescriptions();
+        StageDescriptions.Add(fragmentType, result);
+      }
+      return result;
+    }
+
+    public void Lock()
+    {
+      VerifyNotLocked();
+      Locked = true;
+      foreach (var pair in StageDescriptions)
+        pair.Value.Lock();
+    }
+
+    void VerifyNotLocked()
+    {
+      if (Locked)
+        throw new System.Exception("Cannot add a field to a locked description");
+    }
+  }
+}
---
+++ Compositing/Settings/RenderTargetSettings.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace CSShaders.Compositing
+{
+  /// Settings that describe the render targets that are available to the output of the pixel stage when building a composited shader.
+  public class RenderTargetSettings
+  {
+    public class RenderTarget
+    {
+      public string Name;
+      public ShaderType Type;
+      public int Location;
+
+      public string GetDebugString()
+      {
+        return $"{Type.GetPrettyName()} {Name}";
+      }
+    }
+
+    public List<RenderTarget> RenderTargets = new List<RenderTarget>();
+    public bool Locked { get; private set; }
+
+    /// Adds a new render target with the given type and name. The location must be unique for all render targets.
+    public void AddNamedRenderTarget(ShaderLibrary library, string name, int location)
+    {
+      var vector4Type = library.FindType(new TypeKey(typeof(Math.Vector4)));
+      AddNamedRenderTarget(vector4Type, name, location);
+    }
+
+    /// Adds a new render target with the given type and name. The location must be unique for all render targets.
+    public void AddNamedRenderTarget(ShaderType type, string name, int location)
+    {
+      RenderTargets.Add(new RenderTarget
+      {
+        Type = type,
+        Name = name,
+        Location = location
+      });
+    }
+
+    public void Lock()
+    {
+      VerifyNotLocked();
+      Locked = true;
+      Validate(true);
+    }
+
+    public bool Validate(bool throwException)
+    {
+      bool isValid = true;
+      var usedLocations = new Dictionary<int, RenderTarget>();
+      foreach (var target in RenderTargets)
+      {
+        var usedLocation = usedLocations.GetValueOrDefault(target.Location);
+        if (usedLocation != null)
+        {
+          isValid = false;
+          if (throwException)
+            throw new System.Exception($"Attribute '{target.GetDebugString()}' uses location {target.Location} which is already used by '{usedLocation.GetDebugString()}'");
+        }
+        usedLocations.Add(target.Location, target);
+      }
+      return isValid;
+    }
+
+    void VerifyNotLocked()
+    {
+      if (Locked)
+        throw new System.Exception("RenderTarget settings are locked");
+    }
+  }
+}
